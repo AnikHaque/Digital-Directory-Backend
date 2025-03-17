eacc4e86ef2638088dd1a3401359a11ac321b68a
AnikHaque <ekramul15-811@diu.edu.bd>
2025-03-18 01:01:21 +0600

    delete company done

--- index.js
+++ index.js
@@ -70,6 +70,14 @@ async function run() {
       res.send(result);
     });
 
+    //Delete  data
+    app.delete("/companyDelete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await companyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
