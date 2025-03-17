33bc837512f3d0104830e9cc1dccd849934e47e3
AnikHaque <ekramul15-811@diu.edu.bd>
2025-03-18 00:58:33 +0600

    get a single company done

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
       res.send(result);
     });
 
+    // Get a single company data from db using  id
+    app.get("/company/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await companyCollection.findOne(query);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
